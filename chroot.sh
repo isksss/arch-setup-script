776494b113ab6786de34e9379e2548aef328d8f1
isksss <104404522+isksss@users.noreply.github.com>
2023-05-09 14:40:42 +0900

    add password root

--- chroot.sh
+++ chroot.sh
@@ -20,3 +20,5 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub --re
 mkdir -p /boot/EFI/boot
 cp /boot/EFI/grub/grubx64.efi  /boot/EFI/boot/bootx64.efi
 grub-mkconfig -o /boot/grub/grub.cfg
+
+passwd root
